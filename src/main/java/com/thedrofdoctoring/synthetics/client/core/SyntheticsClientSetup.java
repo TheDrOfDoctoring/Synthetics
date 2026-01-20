63de4b9a81071e3769ed7ea0b9677f5ed0536d8d
ageuxo <ageuxo@gmail.com>
2026-01-20 22:29:24 +0100

    Add InstallableModelLoader for loading InstallableBakedModels and their referenced BakedModels

--- src/main/java/com/thedrofdoctoring/synthetics/client/core/SyntheticsClientSetup.java
+++ src/main/java/com/thedrofdoctoring/synthetics/client/core/SyntheticsClientSetup.java
@@ -1,17 +1,27 @@
 package com.thedrofdoctoring.synthetics.client.core;
 
+import com.mojang.logging.LogUtils;
 import com.thedrofdoctoring.synthetics.Synthetics;
+import com.thedrofdoctoring.synthetics.client.core.assets.InstallableModelLoader;
 import com.thedrofdoctoring.synthetics.client.overlay.EnergyOverlay;
 import com.thedrofdoctoring.synthetics.client.particles.SyntheticsClientParticles;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
+
+import java.util.Collection;
 
 public class SyntheticsClientSetup {
 
     public static void register(IEventBus modbus){
         modbus.addListener(SyntheticsClientSetup::registerOverlays);
         modbus.addListener(SyntheticsEntitiesClient::registerLayers);
+        modbus.addListener(SyntheticsClientSetup::registerClientReloadListeners);
+        modbus.addListener(SyntheticsClientSetup::registerAdditionalModels);
         modbus.addListener(SyntheticsEntitiesClient::registerRenderers);
         modbus.addListener(SyntheticsEntitiesClient::addRenderLayers);
         modbus.addListener(SyntheticsBEClient::registerBlockEntityRenderers);
@@ -24,4 +34,17 @@ public class SyntheticsClientSetup {
         event.registerAbove(VanillaGuiLayers.ARMOR_LEVEL, Synthetics.rl("energy_overlay"), new EnergyOverlay());
 
     }
+
+    public static void registerClientReloadListeners(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(InstallableModelLoader.INSTANCE);
+    }
+
+    public static void registerAdditionalModels(ModelEvent.RegisterAdditional event) {
+        Collection<ResourceLocation> requestedModels = InstallableModelLoader.INSTANCE.requestedModels();
+        LogUtils.getLogger().debug("Registering {} models referenced by InstallableModels", requestedModels.size());
+        for (ResourceLocation model : requestedModels) {
+            event.register(ModelResourceLocation.standalone(model));
+        }
+    }
+
 }
---
+++ src/main/java/com/thedrofdoctoring/synthetics/client/core/assets/InstallableModelLoader.java
@@ -0,0 +1,65 @@
+package com.thedrofdoctoring.synthetics.client.core.assets;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.mojang.logging.LogUtils;
+import com.mojang.serialization.JsonOps;
+import com.thedrofdoctoring.synthetics.client.renderers.installables.InstallableBakedModel;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import org.slf4j.Logger;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.*;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class InstallableModelLoader extends SimpleJsonResourceReloadListener {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final InstallableModelLoader INSTANCE = new InstallableModelLoader();
+
+    private final Map<ResourceLocation, InstallableBakedModel> installableModels = new HashMap<>();
+    private final Set<ResourceLocation> requestedModels = new HashSet<>();
+
+    private InstallableModelLoader() {
+        super(new Gson(), "synthetics/models");
+    }
+
+    @Override
+    protected void apply(Map<ResourceLocation, JsonElement> modelJsons, ResourceManager resourceManager, ProfilerFiller profiler) {
+        installableModels.clear();
+        requestedModels.clear();
+
+        int loaded = 0;
+
+        for (var entry : modelJsons.entrySet()) {
+            try {
+                var installable = InstallableBakedModel.CODEC.parse(JsonOps.INSTANCE, entry.getValue())
+                        .getOrThrow(JsonParseException::new);
+
+                installableModels.put(entry.getKey(), installable);
+                requestedModels.add(installable.model());
+
+                loaded++;
+            } catch (Exception e) {
+                LOGGER.error("Failed loading installable model {}", entry.getKey(), e);
+            }
+        }
+
+        LOGGER.info("Loaded {} installable models", loaded);
+    }
+
+    public InstallableBakedModel get(ResourceLocation location) {
+        return installableModels.get(location);
+    }
+
+    public Collection<ResourceLocation> requestedModels() {
+        return requestedModels;
+    }
+
+}
