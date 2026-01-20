9afa5a4b0e08dc051263f94c4856db73235e993e
ageuxo <ageuxo@gmail.com>
2026-01-20 22:28:33 +0100

    Change model in IInstallableModel to be a ResourceLocation

--- src/main/java/com/thedrofdoctoring/synthetics/client/renderers/installables/IInstallableModel.java
+++ src/main/java/com/thedrofdoctoring/synthetics/client/renderers/installables/IInstallableModel.java
@@ -1,11 +1,10 @@
 package com.thedrofdoctoring.synthetics.client.renderers.installables;
 
-import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.resources.ResourceLocation;
 import org.joml.Vector3fc;
 
 public interface IInstallableModel {
-    BakedModel model();
+    ResourceLocation model();
     Vector3fc translation();
-    String renderLocation();
 
 }
